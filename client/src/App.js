b8863e8190f669faf3751f470448de8470c7d5bb
epiharma <44530473+epiharma@users.noreply.github.com>
2019-05-26 20:13:29 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Login from './components/Login'
+import Register from './components/Register'
+import Landing from './components/Landing'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <Navbar/>
+      <Route exact path="/" component={Landing}/>
+      <div className="container">
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/register" component={Register}/>
+
+      </div>
+      </div>
+    </Router>
   );
+}
 }
 
 export default App;
